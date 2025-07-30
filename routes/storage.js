22efbded069e86fcd2431397f7fafc22168cade1
Max <nikulins.maksims@gmail.com>
2025-07-30 11:14:00 +0300

    Test api routes2

--- routes/storage.js
+++ routes/storage.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { add, edit, remove, all, storedItems } = require("../controllers/storage");
+const { add, edit, remove, all, currentItem } = require("../controllers/storage");
 const { stuffAuth } = require('../middleware/stuffAuth');
 // const { isAdmin } = require('../middleware/isAdmin');
 
 
 router.get("/", stuffAuth, all);
-router.get("/:id", stuffAuth, storedItems);
+router.get("/currentItem", stuffAuth, currentItem);
 router.post("/add", stuffAuth, add);
 router.delete("/remove", stuffAuth, remove);
 router.put("/edit", stuffAuth, edit);
